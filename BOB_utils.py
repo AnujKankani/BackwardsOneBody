6d686927a75111225f3a4d2064de3c4f97e8949a
AnujKankani <kankania00@gmail.com>
2025-10-06 15:48:28 -0400

    NR data initialization support

--- BOB_utils.py
+++ BOB_utils.py
@@ -1447,14 +1447,9 @@ class BOB:
             self.NR_based_on_BOB_ts = self.data.resampled(BOB_ts.t)
 
         return BOB_ts.t,BOB_ts.y
-    def initialize_with_sxs_data(self,sxs_id,l=2,m=2,download=True): 
+    def initialize_with_sxs_data(self,sxs_id,l=2,m=2,download=True,resample_dt = 0.01,verbose=False,inertial_to_coprecessing_transformation=False): 
         '''
         This function is used to initialize the BOB with SXS data.
-        Attributes:
-            t (np.ndarray): Time array
-            h (np.ndarray): Strain data
-            psi4 (np.ndarray): Weyl Scalar (psi_4) data
-            news (np.ndarray): News data
 
         args:
             sxs_id(str): SXS id of the simulation
@@ -1465,6 +1460,10 @@ class BOB:
         print("loading SXS data: ",sxs_id)
         sim = sxs.load(sxs_id,download=download)
         ref_time = sim.metadata.reference_time
+
+        self.resample_dt = resample_dt
+        print("Resampling data to dt = ",self.resample_dt)
+
         self.sxs_id = sxs_id
         self.mf = sim.metadata.remnant_mass
         self.chif = sim.metadata.remnant_dimensionless_spin
@@ -1472,8 +1471,9 @@ class BOB:
         self.M_tot = sim.metadata.reference_mass1 + sim.metadata.reference_mass2
         
         sign = np.sign(self.chif[2])
-        #if(np.abs(self.chif[0])>0.01 or np.abs(self.chif[1])>0.01):
-        #    raise ValueError("Final spin has non-zero x or y component for "+sxs_id+" This is not supported currently")
+        if(np.abs(self.chif[0])>0.01 or np.abs(self.chif[1])>0.01):
+           print("Warning: Final spin has non-zero x or y component for "+sxs_id+". Precessing cases have not beent tested yet. Proceed at your own risk!")
+
         self.chif = np.linalg.norm(self.chif)
         self.chif_with_sign = self.chif*sign
         self.Omega_ISCO = gen_utils.get_Omega_isco(self.chif,self.mf)
@@ -1485,26 +1485,39 @@ class BOB:
         self.tau = np.abs(tau)
         self.Omega_QNM = self.w_r/np.abs(self.m)
 
+        
+
         h = sim.h
         h = h.interpolate(np.arange(h.t[0],h.t[-1],self.resample_dt))
         if(inertial_to_coprecessing_transformation):
+            print("Converting from inertial to coprecessing frame!")
             h = h.to_coprecessing_frame().copy()
+
         hm = gen_utils.get_kuibit_lm(h,self.l,self.m).cropped(init=ref_time+100)
         #we also store the (l,-m) mode for current and quadrupole wave construction
         hmm = gen_utils.get_kuibit_lm(h,self.l,-self.m).cropped(init=ref_time+100)
-        peak_strain_time = hm.time_at_maximum()
-        self.strain_tp = peak_strain_time
+        tp,Ap = gen_utils.get_tp_Ap_from_spline(hm.abs())
+        self.strain_tp = tp
+        self.strain_Ap = Ap
+        
         self.h_L2_norm_tp = h.max_norm_time()
 
         psi4 = sim.psi4
         psi4 = psi4.interpolate(np.arange(h.t[0],h.t[-1],self.resample_dt))
+        if(inertial_to_coprecessing_transformation):
+            print("Converting from inertial to coprecessing frame!")
+            psi4 = psi4.to_coprecessing_frame().copy()
         psi4m = gen_utils.get_kuibit_lm_psi4(psi4,self.l,self.m).cropped(init=ref_time+100)
         psi4mm = gen_utils.get_kuibit_lm_psi4(psi4,self.l,-self.m).cropped(init=ref_time+100)
-        self.psi4_tp = psi4m.time_at_maximum()
+        tp,Ap = gen_utils.get_tp_Ap_from_spline(psi4m.abs())
+        self.psi4_tp = tp
+        self.psi4_Ap = Ap
 
         newsm = hm.spline_differentiated(1)
         newsmm = hmm.spline_differentiated(1)
-        self.news_tp = newsm.time_at_maximum()
+        tp,Ap = gen_utils.get_tp_Ap_from_spline(newsm.abs())
+        self.news_tp = tp
+        self.news_Ap = Ap
 
         self.strain_data = hm
         self.full_strain_data = h
@@ -1516,43 +1529,60 @@ class BOB:
         self.psi4_data = psi4m
         self.full_psi4_data = psi4
         self.psi4_mm_data = psi4mm
-    def initialize_with_cce_data(self,cce_id,l=2,m=2,perform_superrest_transformation=False,inertial_to_coprecessing_transformation=False):
+
+        if(verbose):
+            print("Mtot = ",self.M_tot)
+            print("Mf = ",self.mf)
+            print("chif = ",self.chif_with_sign)
+            print("requested (l,m) = (",self.l,",",self.m,")")
+            print("Omega_ISCO = ",self.Omega_ISCO)
+            print("Omega_QNM = ",self.Omega_QNM)
+            print("tau = ",self.tau)
+            print("h_L2_norm_tp = ",self.h_L2_norm_tp)
+            print("strain_tp = ",self.strain_tp)
+            print("strain_Ap = ",self.strain_Ap)
+            print("news_tp = ",self.news_tp)
+            print("news_Ap = ",self.news_Ap)
+            print("psi4_tp = ",self.psi4_tp)
+            print("psi4_Ap = ",self.psi4_Ap)
+            
+    def initialize_with_cce_data(self,cce_id,l=2,m=2,perform_superrest_transformation=False,inertial_to_coprecessing_transformation=False,provide_own_abd=None,resample_dt = 0.01,verbose=False):
         '''
         This function is used to initialize the BOB with CCE data.
-        Attributes:
-            t (np.ndarray): Time array
-            h (np.ndarray): Strain data
-            psi4 (np.ndarray): Weyl Scalar (psi_4) data
-            news (np.ndarray): News data
-
 
         args:
-            cce_id(str): CCE id of the simulation
+            cce_id(str): CCE id of the simulation (https://data.black-holes.org/waveforms/extcce_catalog.html)
             l(int): Mode number
             m(int): Mode number
             perform_superrest_transformation(bool): Whether to perform a superrest transformation
             inertial_to_coprecessing_transformation(bool): Whether to perform an inertial to coprecessing transformation
+            provide_own_abd: Use a user passed in scri abd object (maybe useful if the user has specific pre-processing requirements)
         '''
         import qnmfits #adding here so this code can be used without WSL for non-cce purposes
-        print("loading CCE data")
+        print("loading CCE id",cce_id)
+
         if(provide_own_abd is None):
             abd = qnmfits.cce.load(cce_id)
         else:
             abd = provide_own_abd
+        print("resampling CCE data to dt = ",self.resample_dt)
+
         if(perform_superrest_transformation):
-            print("performing superrest transformation")
-            print("this may take ~20 minutes the first time")
+            print("Performing superrest transformation")
+            print("This may take ~20 minutes the first time")
             # We can extract individual spherical-harmonic modes like this:
             h = abd.h
             h22 = h.data[:,h.index(2,2)]
             h.t -= h.t[np.argmax(np.abs(h22))]
             abd = qnmfits.utils.to_superrest_frame(abd, t0 = 300)
-
-
+        self.metadata = abd.metadata
+        #note, the final system may be in a different frame than the initial system if the superrest transformation is performed
+        self.M_tot = self.metadata['reference_mass1'] + self.metadata['reference_mass2']
         self.mf = abd.bondi_rest_mass()[-1]
         self.chif = abd.bondi_dimensionless_spin()[-1]
         if(np.abs(self.chif[0])>0.01 or np.abs(self.chif[1])>0.01):
-            raise ValueError("Final spin has non-zero x or y component for "+cce_id+" This is not supported currently")
+            print("Warning: This may be a precessing case, which this code has not been tested for yet. Procceed at your own risk!")
+        
         sign = np.sign(self.chif[2])
         self.chif = np.linalg.norm(self.chif)
         self.chif_with_sign = self.chif*sign
@@ -1560,18 +1590,25 @@ class BOB:
         self.Omega_0 = self.Omega_ISCO
         self.l = l
         self.m = m
+        
         w_r,tau = gen_utils.get_qnm(self.chif,self.mf,self.l,np.abs(self.m),n=0,sign=sign)
         self.w_r = np.abs(w_r)
         self.tau = np.abs(tau)
         self.Omega_QNM = self.w_r/np.abs(self.m)
+        
+        # if a superrest transform is performed then this will be the superrest abd
         h = abd.h.interpolate(np.arange(abd.h.t[0],abd.h.t[-1],self.resample_dt))
+        
         if(inertial_to_coprecessing_transformation):
+            if(perform_superrest_transformation):
+                print("Warning, you have performed a superrest transformation and an inertial to coprecessing transformation. This may not be what you want!")    
             print("converting to coprecessing frame!")
             h = h.to_coprecessing_frame()
+
         self.strain_scri_wm = h.copy()
         sxs_h_waveform = h.copy().to_sxs #convert scri wavefrom mode to a sxs waveform mode
-
         self.strain_wm = sxs_h_waveform
+
         if(perform_superrest_transformation):
             #superrest cuts off a lot of the inspiral data. By default max_norm_time ignores the first 1/4 of the data
             #so for the superrest case, this removes the actual peak
@@ -1581,8 +1618,14 @@ class BOB:
 
         hm = gen_utils.get_kuibit_lm(h,self.l,self.m)
         hmm = gen_utils.get_kuibit_lm(h,self.l,-self.m)
-        #TODO: psi4 is not affected by frame transformations
+        
         psi4 = abd.psi4.interpolate(np.arange(abd.h.t[0],abd.h.t[-1],self.resample_dt))
+        if(inertial_to_coprecessing_transformation):
+            if(perform_superrest_transformation):
+                print("Warning, you have performed a superrest transformation and an inertial to coprecessing transformation. This may not be what you want!")
+            print("converting to coprecessing frame!")
+            psi4 = psi4.to_coprecessing_frame()
+        
         psi4m = gen_utils.get_kuibit_lm_psi4(psi4,self.l,self.m)
         psi4mm = gen_utils.get_kuibit_lm_psi4(psi4,self.l,-self.m)
 
@@ -1609,9 +1652,34 @@ class BOB:
         self.strain_mm_data = hmm
         self.news_mm_data = newsmm
         self.psi4_mm_data = psi4mm
-    def initialize_with_NR_psi4_data(self,t,y,mf,chif,l=2,m=2):
+
+        if(verbose):
+            print("Mtot = ",self.M_tot)
+            if(perform_superrest_transformation):
+                print("Bondi Mf = ",self.mf)
+            else:
+                print("Mf = ",self.mf)
+            if(perform_superrest_transformation):
+                print("Bondi chif = ",self.chif_with_sign)
+            else:
+                print("chif = ",self.chif_with_sign)
+                
+            print("requested (l,m) = (",self.l,",",self.m,")")
+            print("Omega_ISCO = ",self.Omega_ISCO)
+            print("Omega_QNM = ",self.Omega_QNM)
+            print("tau = ",self.tau)
+            print("h_L2_norm_tp = ",self.h_L2_norm_tp)
+            print("strain_tp = ",self.strain_tp)
+            print("strain_Ap = ",self.strain_Ap)
+            print("news_tp = ",self.news_tp)
+            print("news_Ap = ",self.news_Ap)
+            print("psi4_tp = ",self.psi4_tp)
+            print("psi4_Ap = ",self.psi4_Ap)
+
+    def initialize_with_NR_mode(self,t_NR,y_psi4,y_strain,mf,chif,l=2,m=2,w_r = -1,tau = -1,verbose=False):
+        raise ValueError("This function is not operational yet!")
         '''
-        This function is used to initialize the BOB with NR psi4 data.
+        This function is used to initialize the BOB with NR data. Currently this function only supports the input of one (s=-2,l,m) mode.
         Attributes:
             tau(float): Damping term; can also be described as 1/gamma (gamma is imaginry QNM fre)
             Ap(float): Amplitude at time of peak amplitude
@@ -1625,27 +1693,70 @@ class BOB:
             l(int): Mode number
             m(int): Mode number
         '''
-        ts = kuibit_ts(t,y)
+        if(len(t_NR)!=len(y_psi4)):
+            raise ValueError("t_NR and y_psi4 must have the same length")
+        if(len(t_NR)!=len(y_strain)):
+            raise ValueError("t_NR and y_strain must have the same length")
+
+        if(l!=abs(m)):
+            print("Warning! l != abs(m). This is not supported currently. Proceed at your own risk!")
+        
+        ts_psi4 = kuibit_ts(t_NR,y_psi4)
+        ts_strain = kuibit_ts(t_NR,y_strain)
+        
         self.mf = mf
         self.chif = chif
+
         if(np.abs(self.chif[0])>0.01 or np.abs(self.chif[1])>0.01):
-            raise ValueError("Final spin has non-zero x or y component for this data. This is not supported currently")
+            raise ValueError("Final spin has non-zero x or y component for this data. This is not supported currently for NR data")
+        
         sign = np.sign(self.chif[2])
         self.chif = np.linalg.norm(self.chif)
         self.l = l
         self.m = m
+        
         self.Omega_ISCO = gen_utils.get_Omega_isco(self.chif,self.mf)
         self.Omega_0 = self.Omega_ISCO
-        w_r,tau = gen_utils.get_qnm(self.chif,self.mf,self.l,np.abs(self.m),n=0,sign=sign)
-        self.w_r = np.abs(w_r)
-        self.tau = np.abs(tau)
+        
+        if(w_r<0 or tau<0):
+            print("Calculating Kerr QNM parameters from provided Mf and chif")
+            w_r,tau = gen_utils.get_qnm(self.chif,self.mf,self.l,np.abs(self.m),n=0,sign=sign)
+            self.w_r = np.abs(w_r)
+            self.tau = np.abs(tau)
+        else:
+            print("Using user provided w_r and tau!")
+            self.w_r = np.abs(w_r)
+            self.tau = np.abs(tau)
+        
         self.Omega_QNM = self.w_r/np.abs(self.m)
-        self.psi4_data = ts
-        #self.data = self.psi4_data
-        self.Ap = self.psi4_data.abs_max()
-        #self.tp = self.psi4_data.time_at_maximum()
-        self.psi4_tp = self.psi4_data.time_at_maximum()
+        self.psi4_data = ts_psi4
+        tp,Ap = gen_utils.get_tp_Ap_from_spline(ts_psi4.abs())
+        self.psi4_tp = tp
+        self.psi4_Ap = Ap
+        self.strain_data = ts_strain
+        tp,Ap = gen_utils.get_tp_Ap_from_spline(ts_strain.abs())
+        self.strain_tp = tp
+        self.strain_Ap = Ap
+
+        ts_news = ts_strain.spline_differentiated(1)
+        tp,Ap = gen_utils.get_tp_Ap_from_spline(ts_news.abs())
+        self.news_tp = tp
+        self.news_Ap = Ap
+        self.news_data = ts_news
+
+        print("requested (l,m) = (",self.l,",",self.m,")")
+        print("Omega_ISCO = ",self.Omega_ISCO)
+        print("Omega_QNM = ",self.Omega_QNM)
+        print("tau = ",self.tau)
+        print("strain_tp = ",self.strain_tp)
+        print("strain_Ap = ",self.strain_Ap)
+        print("news_tp = ",self.news_tp)
+        print("news_Ap = ",self.news_Ap)
+        print("psi4_tp = ",self.psi4_tp)
+        print("psi4_Ap = ",self.psi4_Ap)
+
     def initialize_manually(self,mf,chif,l,m,**kwargs):
+        raise ValueError("This function is not operational yet.")
         '''
         This function is used to initialize the BOB manually.
         
@@ -1673,7 +1784,8 @@ class BOB:
             setattr(self, key, value)
     def get_psi4_data(self,**kwargs):
         '''
-        This function is used to get the psi4 data.
+        This function is used to get the NR psi4 data.
+        By default it will return the (l,m) mode specified during BOB initialization but l & m can be specified by the user.
         
         args:
             l(int): Mode number
@@ -1691,7 +1803,8 @@ class BOB:
         return temp_ts.t,temp_ts.y
     def get_news_data(self,**kwargs):
         '''
-        This function is used to get the news data.
+        This function is used to get the NR news data.
+        By default it will return the (l,m) mode specified during BOB initialization but l & m can be specified by the user.
         
         args:
             l(int): Mode number
@@ -1709,7 +1822,8 @@ class BOB:
         return temp_ts.t,temp_ts.y
     def get_strain_data(self,**kwargs):
         '''
-        This function is used to get the strain data.
+        This function is used to get the NRstrain data. 
+        By default it will return the (l,m) mode specified during BOB initialization but l & m can be specified by the user.
         
         args:
             l(int): Mode number
@@ -1728,8 +1842,4 @@ class BOB:
 #convenience class for template generation
 
 if __name__=="__main__":
-    #pass
-    #print("Welcome to the Wonderful World of BOB!! All Hail Our Glorius Leader Sean! (totally not a cult)")
-    #welcome_to_BOB()
-    #print_sean_face()
-    test_phase_freq_finite_t0()
+    print("Howdy!")
