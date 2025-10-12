801696c894b5d5df7dbe9c8f11a8733eb8ae312d
AnujKankani <kankania00@gmail.com>
2025-10-12 12:57:42 -0400

    Add sphinx docs

---
+++ docs/source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))  # for ReadTheDocs (root)
+sys.path.insert(0, os.path.abspath('../'))
+project = 'gwBOB'
+copyright = '2025, Anuj Kankani'
+author = 'Anuj Kankani'
+release = '0.1.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.autosummary",
+    "sphinx_autodoc_typehints",
+    "myst_parser",
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+source_suffix = [".rst", ".md"]
+autosummary_generate = True
