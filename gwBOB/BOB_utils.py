1c6e0fb9f0eec059ac37e7b713ba92060bddedfb
Anuj Kankani <kankania00@gmail.com>
2026-02-20 10:07:41 -0500

    Add missing init and CCE resample_dt fixes

--- gwBOB/BOB_utils.py
+++ gwBOB/BOB_utils.py
@@ -63,6 +63,7 @@ class BOB:
         self.tp = 0
         
         self.what_is_BOB_building="Nothing"
+        self.__what_to_create = "Nothing"
         self.l = 2
         self.m = 2
         self.Phi_0 = 0
@@ -1123,6 +1124,8 @@ class BOB:
             raise ValueError("m=0 case not implemented yet")
         import qnmfits #adding here so this code can be used without WSL for non-cce purposes
         print("loading CCE id",cce_id)
+
+        self.resample_dt = resample_dt
 
         if(provide_own_abd is None):
             abd = qnmfits.cce.load(cce_id)
